5f480eb8811b6bb14d6d6625d32eeed44808e504
YasorI13 <74849918+YasorI13@users.noreply.github.com>
2025-06-18 12:59:31 +0700

    fix build: mark emd page as dynamic

--- src/app/(toolsList)/emd/page.tsx
+++ src/app/(toolsList)/emd/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+
+export const dynamic = 'force-dynamic';
 import prisma from "@/lib/db";
 import TableCostCtr from '../components/TableCostCtr';
 import { CostCtr } from '@prisma/client';
